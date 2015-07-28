5983caf6c501edecc4d760666ccaf45a9a59628b
Edward Liaw <ed.liaw@gmail.com>
2015-07-28 10:48:05 -0700

    Implement inefficient randomized quicksort

--- hw7/HW07.hs
+++ hw7/HW07.hs
@@ -11,6 +11,7 @@ import Data.Functor
 import Data.Monoid
 import Data.Vector (Vector, cons, (!), (!?), (//))
 import System.Random
+import Data.Maybe
 
 import qualified Data.Vector as V
 
@@ -18,43 +19,67 @@ import qualified Data.Vector as V
 -- Exercise 1 -----------------------------------------
 
 liftM :: Monad m => (a -> b) -> m a -> m b
-liftM = undefined
+
+liftM f mx = do
+    x <- mx
+    return $ f x
+--liftM f mx = mx >>= \x -> return $ f x
 
 swapV :: Int -> Int -> Vector a -> Maybe (Vector a)
-swapV = undefined
+swapV i j vx = liftM2 swap (vx !? i) (vx !? j)
+  where swap x y = vx // [(i, y), (j, x)]
 
 -- Exercise 2 -----------------------------------------
 
 mapM :: Monad m => (a -> m b) -> [a] -> m [b]
-mapM = undefined
+mapM f = sequence . map f
 
 getElts :: [Int] -> Vector a -> Maybe [a]
-getElts = undefined
+getElts xs vx = mapM (vx !?) xs
 
 -- Exercise 3 -----------------------------------------
 
 type Rnd a = Rand StdGen a
 
 randomElt :: Vector a -> Rnd (Maybe a)
-randomElt = undefined
+randomElt vx = do
+    r <- getRandomR (0, V.length vx - 1)
+    return $ vx !? r
 
 -- Exercise 4 -----------------------------------------
 
 randomVec :: Random a => Int -> Rnd (Vector a)
-randomVec = undefined
+randomVec n = sequence $ V.replicate n getRandom
 
 randomVecR :: Random a => Int -> (a, a) -> Rnd (Vector a)
-randomVecR = undefined
+randomVecR n b = sequence . V.replicate n $ getRandomR b
 
 -- Exercise 5 -----------------------------------------
 
 shuffle :: Vector a -> Rnd (Vector a)
-shuffle = undefined
+shuffle vx = swapRandom (V.length vx - 1) vx
+  where
+    swapRandom :: Int -> Vector a -> Rnd (Vector a)
+    swapRandom 0 vx' = return vx'
+    swapRandom i vx' = do
+      j <- getRandomR (0, i)
+      let vx'' = fromJust $ swapV i j vx'
+      swapRandom (i - 1) vx''
 
 -- Exercise 6 -----------------------------------------
 
+pop :: Vector a -> Int -> (a, Vector a)
+pop vx i = (V.head rest, before <> (V.tail rest))
+    where
+      (before, rest) = V.splitAt i vx
+
 partitionAt :: Ord a => Vector a -> Int -> (Vector a, a, Vector a)
-partitionAt = undefined
+partitionAt vx i = (lt, x, ge)
+    where 
+          x = vx ! i
+          (before, after') = V.splitAt i vx
+          after = V.tail after'
+          (lt, ge) = V.unstablePartition (< x) (before V.++ after)
 
 -- Exercise 7 -----------------------------------------
 
@@ -65,12 +90,23 @@ quicksort (x:xs) = quicksort [ y | y <- xs, y < x ]
                    <> (x : quicksort [ y | y <- xs, y >= x ])
 
 qsort :: Ord a => Vector a -> Vector a
-qsort = undefined
+qsort vx
+  | V.null vx = vx
+  | otherwise = qsort [ x | x <- vx', x < x' ]
+                <> V.singleton x'
+                <> qsort [ x | x <- vx', x >= x' ]
+                where vx' = V.tail vx
+                      x' = V.head vx
 
 -- Exercise 8 -----------------------------------------
 
 qsortR :: Ord a => Vector a -> Rnd (Vector a)
-qsortR = undefined
+qsortR vx = do
+    r <- getRandomR (0, V.length vx - 1)
+    let (lt, p, gt) = partitionAt vx r
+    lt' <- qsortR lt
+    gt' <- qsortR gt
+    return $ lt' <> V.singleton p <> gt'
 
 -- Exercise 9 -----------------------------------------
 
